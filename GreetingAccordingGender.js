01da6dfce5a03437cc1b3ba9eb43507548db519c
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-08-05 20:42:42 +0530

    Create GreetingAccordingGender.js
    Greeting someone with respect to there gender.

---
+++ GreetingAccordingGender.js
@@ -0,0 +1,9 @@
+let firstName = "Steve";
+let lastName = "jobs";
+let Gender = "male";
+if (Gender="male"){
+    console.log("Greetings Mr. " + firstName + " " + lastName);
+}
+else{
+    console.log("Greetings Miss " + firstName + " " + lastName);
+}
